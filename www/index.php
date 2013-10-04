8649c67110ca2a50785a76ba7a982c754be0944a
Dave Hulbert <dave1010@gmail.com>
2013-10-04 22:31:18 +0100

    Route for homepage

--- www/index.php
+++ www/index.php
@@ -5,7 +5,14 @@ require_once __DIR__.'/../app/bootstrap.php';
 
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
+$app->get('/', function() {
+    $json = getWordPressJson('?json=1');
+    $posts = $json->posts;
+    $post = $posts[0];
+    return "<h1>$post->title</h1>" .  "<div>$post->content</div>";
+});
 
 $app->run();
 
