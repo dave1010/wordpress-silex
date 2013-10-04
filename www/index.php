4629605441c957d0dd3486331f60bc3daff03643
Dave Hulbert <dave1010@gmail.com>
2013-10-04 22:45:41 +0100

    Load all pages from WP

--- www/index.php
+++ www/index.php
@@ -14,5 +14,19 @@ $app->get('/', function() {
     return "<h1>$post->title</h1>" .  "<div>$post->content</div>";
 });
 
+$app->get('{url}', function($url) {
+//    var_dump($url);die();
+    $json = getWordPressJson($url.'?json=1');
+
+//var_dump($json);die();
+    $post = $json->page;
+//    $post = $posts[0];
+    return "<h1>$post->title</h1>" .  "<div>$post->content</div>";
+});
+
+
+
+
+
 $app->run();
 
